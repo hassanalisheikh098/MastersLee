0685aaca5165e4a28665a3942fca0805ff9eacbc
hassanalisheikh098 <143602821+hassanalisheikh098@users.noreply.github.com>
2025-03-01 00:00:32 +0500

    Update Viewmore.jsx

--- src/components/Viewmore.jsx
+++ src/components/Viewmore.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { fetchPrograms } from "../data/programs";
+import { fetchPrograms } from "../data/Programs";
 
 export default function UniversityPage() {
   const { id } = useParams();
