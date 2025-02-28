9b451dea970ed1ede2b75f714aa9009595d01438
hassanalisheikh098 <143602821+hassanalisheikh098@users.noreply.github.com>
2025-02-28 23:58:54 +0500

    Update Programs.js

--- src/data/Programs.js
+++ src/data/Programs.js
@@ -29,10 +29,10 @@ export const fetchPrograms = async () => {
   }));
 };
 
-<<<<<<< HEAD
-=======
+
+
 // Usage example within a function (since top-level await may not work in this context)
->>>>>>> 1ce6d02 (Updated vite.config.js and index.html)
+
 async function fetchData() {
   const programs = await fetchPrograms();
   console.log(programs);
